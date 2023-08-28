2d19c4efc491b16dda10d758fbf24379ff8a2326
rudra <m.rudra2003@gmail.com>
2023-08-29 00:19:03 +0530

    db

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/cloudnotebook';
 
 const connectToMongo = () => {
-
-    mongoose.connect('mongodb://127.0.0.1:27017/cloudnotebook')
+    mongoose.connect(dbUrl)
         .then(() => {
             console.log("CONNECTION OPEN!!!")
         })
