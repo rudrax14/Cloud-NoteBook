1025f673f69e8f0ab00c7c1613da3a28ba84eba2
rudrax14 <m.rudra2003@gmail.com>
2024-03-04 02:00:37 +0530

    Add CORS support to backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require("dotenv").config()
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
